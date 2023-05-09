4a9ad6c58cd4323a5d349e25308b6d943a3041ba
AMIV <everydamnedday@gmail.com>
2023-05-09 14:25:51 -0700

    commit before moving onto unit testing

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,6 @@
 
 import logo5 from '../assets/logo_shadow.png'
+import organicSeal from '../assets/organicSeal.png'
 
 
 
@@ -7,7 +8,7 @@ function Text() {
     const text = 'Il brano standard del Lorem Ipsum usato sin dal sedicesimo secolo è riprodotto qui di seguito per coloro che fossero interessati. Anche le sezioni 1.10.32 e 1.10.33 del "de Finibus Bonorum et Malorum" di Cicerone sono riprodotte nella loro forma originale, accompagnate dalla traduzione inglese del 1914 di H. Rackham.'
     return (
         <>
-            <p className='sm:row-start-1 row-start-2 col-start-2 col-end-4 col-span-3 p-4 text-black mt-64 sm:mt-0 sm:text-white font-medium'>{text}</p>
+            <p className='sm:row-start-1 row-start-2 col-start-2 xl:mr-96 col-end-4 col-span-3 p-4 text-black mt-64 sm:mt-0 sm:text-white font-medium'>{text}</p>
         </>
     )
 }
@@ -15,16 +16,17 @@ function Text() {
 function Title() {
     return (
         <>
-        <div className='row-start-1 xl:col-start-2 col-start-1 sm:col-start-2 col-end-5 col-span-3 sm:pb-5 mx-auto'>
-            <p className='text-5xl lg:text-8xl md:text-7xl text-black font-extrabold pb-5 flex-row hidden sm:flex'>Little Lem
-            <span className=''>
-                <img className='sm:w-10 md:w-12 lg:w-16 sm:mt-1 md:mt-3 lg:mt-4 rounded-2xl sm:opacity-100 hover:duration-200 hover:scale-125' src={logo5} alt="" />
-                </span>n
-            </p>
-            <p className='text-5xl sm:text-8xl text-black font-extrabold pb-5 block sm:hidden'>Little Lemon</p>
-            <p className='italic text-black px-5 p-1 border border-black border-dashed rounded-xl w-fit'>Healthy Cuisine<span className='text-xs text-yellow-300'><br />With a twist</span></p>
-            <Text />
-        </div>
+            <div className='row-start-1 xl:col-start-2 col-start-1 sm:col-start-2 col-end-5 col-span-3 sm:pb-5 mx-auto'>
+                <p className='text-5xl lg:text-8xl md:text-7xl text-black font-extrabold pb-5 flex-row hidden sm:flex'>Little Lem
+                <span className=''>
+                    <img className='sm:w-10 md:w-12 lg:w-16 sm:mt-1 md:mt-3 lg:mt-4 rounded-2xl sm:opacity-100 hover:duration-200 hover:scale-125' src={logo5} alt="" />
+                    </span>n
+                </p>
+                <p className='text-5xl sm:text-8xl text-black font-extrabold pb-5 p-3 block sm:hidden'>Little Lemon</p>
+                <p className='italic text-black ml-3 px-5 p-1 border border-black border-dashed rounded-xl w-fit'>Healthy Cuisine<span className='text-xs text-yellow-300'><br />With a twist</span></p>
+                <Text />
+            </div>
+            <div className='col-start-4 col-end-5 row-start-1 my-auto mx-auto hidden xl:block'><img className='w-48' src={organicSeal} alt="" /></div>
         </>
     )
 }
--- src/UnderConstruction.jsx
+++ src/UnderConstruction.jsx
@@ -1,18 +1,20 @@
 import { Link } from "react-router-dom"
-import Header from "./Components/Header"
-import Nav from "./Components/Nav"
+import image1 from './assets/lemon_helmet.png';
+import image2 from './assets/lemon_helmet_variation.png';
 
 
 export default () => {
     return (
         <>
             
-            <div className="w-screen h-screen bg-gradient-to-br from-yellow-300 via-pink-300 to-lime-300 flex flex-col items-center justify-center">
-                <h1 className="text-center text-6xl text-black font-extrabold italic animate-fade">
+            <div className="grid grid-cols-1 md:grid-cols-3 grid-rows-3 w-full h-screen bg-gradient-to-br from-yellow-300 via-pink-300 to-lime-300 ">
+                <h1 className="z-10 text-center text-2xl md:text-6xl my-auto text-black font-extrabold italic animate-fade col-start-1 col-end-2 md:col-end-4 row-start-2">
                     This page is under construction!
                 </h1>
+                <img className="w-36 md:w-72 p-2 row-start-3 mr-auto md:mx-auto md:my-auto rounded-full col-start-1 col-end-2" src={image1} alt="" />
+                <img className="w-36 md:w-72 p-2 row-start-3 col-start-1 md:col-start-3 md:row-start-1 md:row-end-2 ml-auto md:my-auto md:mx-auto rounded-full md:row-span-2 " src={image2} alt="" />
                 
-                <div className="m-5">
+                <div className="m-5 row-start-1 col-start-1 col-end-2 md:col-start-2 mx-auto my-auto">
                 <span className="italic text-2xl font-bold">Go To:</span>
                     <Link to={'/'}><p className="underline text-2xl font-bold hover:text-lime-300 my-5">Home</p></Link>
                     <Link to={'/Reservations'}><p className="underline text-2xl font-bold hover:text-lime-300">Reservations</p></Link>
@@ -21,3 +23,6 @@ export default () => {
         </>
     )
 }
+
+
+// flex flex-col items-center justify-center
